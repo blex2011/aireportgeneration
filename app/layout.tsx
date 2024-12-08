5cdbf4727e675cdb09a933dc5ebe00c877434947
blex2011 <git@weinerfamily.org>
2024-12-07 23:08:22 -0500

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from 'sonner'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster 
+          position="top-right"
+          closeButton
+          richColors
+          expand={true}
+          visibleToasts={5}
+        />
         {children}
       </body>
     </html>
